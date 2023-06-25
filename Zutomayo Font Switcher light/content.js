1b4d4cf8a629ebb74e0759a46bd41baa916b5d05
tachibana2023 <121209256+geum-ztmy@users.noreply.github.com>
2023-06-25 19:40:02 +0900

    v1.3
    対象範囲拡大

--- Zutomayo Font Switcher light/content.js
+++ Zutomayo Font Switcher light/content.js
@@ -8,10 +8,14 @@ function ChangeFont() {
     const fontFace = new FontFace('ztmy_font', `url(${chrome.runtime.getURL('ZTMY_MOJI-R.otf')})`);
     document.fonts.add(fontFace);
 
-    const elements = document.querySelectorAll('body *');
-    for (let element of elements) {
-        if (element.children.length === 0 && element.textContent.trim() !== '') {
-            element.style.fontFamily = 'ztmy_font';
-        }
-    }
+    // const elements = document.querySelectorAll('body *');
+    // for (let element of elements) {
+    //     if (element.children.length === 0 && element.textContent.trim() !== '') {
+    //         element.style.fontFamily = 'ztmy_font';
+    //     }
+    // }
+
+    const style = document.createElement('style');
+    style.innerHTML = `* { font-family: ztmy_font !important; }`;
+    document.head.appendChild(style);
 }
